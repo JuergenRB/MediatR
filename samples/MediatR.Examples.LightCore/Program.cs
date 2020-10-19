34dcfacfe5e6c2c83ae4d9d8d4b87e4c155f00d3
JuergenRB <22403655+JuergenRB@users.noreply.github.com>
2020-10-19 15:44:12 -0700

    Added LightCore Sample with MediatR.LightCore 0.1.0

---
+++ samples/MediatR.Examples.LightCore/Program.cs
@@ -0,0 +1,33 @@
+﻿using LightCore;
+using MediatR;
+using MediatR.LightCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatR.Examples.LightCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var writer = new WrappingWriter(Console.Out);
+            var mediator = BuildMediator(writer);
+
+            Runner.Run(mediator, writer, "LightCore").Wait();
+        }
+
+        private static IMediator BuildMediator(WrappingWriter writer)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new MediatRModule(typeof(Runner).Assembly));
+
+            builder.Register<System.IO.TextWriter>(ctn => writer);
+            var container = builder.Build();
+
+            return container.Resolve<IMediator>();
+        }
+    }
+}
